49dfc315b2c3e495c56507821dabe30bd4be812a
es <esrazengn00@gmail.com>
2019-12-05 17:14:57 +0100

    Update React-exercises (FriendProfile.js)

--- week2/react-exercises/src/components/friend/FriendProfile.js
+++ week2/react-exercises/src/components/friend/FriendProfile.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import uuid from 'uuid';
 
 function FriendProfile({ friend }) {
   const isObjectEmpty = obj => Object.getOwnPropertyNames(obj).length < 1;
@@ -10,13 +9,11 @@ function FriendProfile({ friend }) {
         ''
       ) : (
         <ul>
-          <li key={uuid()}>{`${friend.name.first} ${friend.name.last}`}</li>
-          <li
-            key={uuid()}
-          >{`${friend.location.street.name} ${friend.location.street.number}, ${friend.location.postcode} 
+          <li>{`${friend.name.first} ${friend.name.last}`}</li>
+          <li>{`${friend.location.street.name} ${friend.location.street.number}, ${friend.location.postcode} 
                 ${friend.location.city}, ${friend.location.country}`}</li>
-          <li key={uuid()}>{friend.email}</li>
-          <li key={uuid()}>{friend.cell}</li>
+          <li>{friend.email}</li>
+          <li>{friend.cell}</li>
         </ul>
       )}
     </div>
